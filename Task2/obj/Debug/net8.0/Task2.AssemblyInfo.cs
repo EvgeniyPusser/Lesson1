a26c7e958eb1cadfc37ea541fb831300db57f250
Evgeniy Pusser <evgeniipusser88@gmail.com>
2024-02-04 15:07:40 +0300

    Quotients

--- Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bce142ef10cb7acda5b2d5acd78b162e7895b6c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+307bad483adb85ef1d9d6e6229dd6204b20163fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
