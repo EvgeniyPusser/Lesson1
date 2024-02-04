a29a3faecd3b8a5feb7cb92e6e02ff68d919ec4b
Evgeniy Pusser <evgeniipusser88@gmail.com>
2024-02-04 22:45:43 +0300

    Condition2

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 33;
+int b = 21;
+int c = 1;
+int d = 0;
+int e = 23;
+
+int max = a;
+    
+if (b > max)
+{
+    max = b;    
+}
+if (c > max)
+{
+    max = c;    
+}
+if (d > max)
+{
+    max = d;    
+}
+if (e > max)
+{
+    max = e;    
+}
+Console.WriteLine(max);
